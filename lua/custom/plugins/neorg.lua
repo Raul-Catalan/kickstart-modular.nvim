2e0996e3e7960c7b860d0d7d704386d604a6c83f
Raul Catalan <raulrcatalan@gmail.com>
2025-08-16 20:41:26 -0700

    Update Neorg Plugin

--- lua/custom/plugins/neorg.lua
+++ lua/custom/plugins/neorg.lua
@@ -25,5 +25,11 @@ return {
     }
     vim.wo.foldlevel = 99
     vim.wo.conceallevel = 2
+
+    vim.keymap.set('n', '<leader>jt', ':Neorg journal today<cr>', { desc = '[T]odays [J]ournal' })
+    vim.keymap.set('n', '<leader>jf', ':Neorg journal tomorrow<cr>', { desc = '[F]uture [J]ournal' })
+    vim.keymap.set('n', '<leader>jy', ':Neorg journal yesterday<cr>', { desc = '[Y]esterday [J]ournal' })
+    vim.keymap.set('n', '<leader>ji', ':Neorg inject-metadata<cr>', { desc = '[I]nject Metadata' })
+    -- vim.keymap.set('n', '<leader>sk', ':Neorg journal today' , { desc = '[T]odays [J]ournal' })
   end,
 }
